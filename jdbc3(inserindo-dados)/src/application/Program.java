dc470033a322eee66ef7ab3923724a4647b64ed7
Herick Exterkoetter <herickherick47@gmail.com>
2020-10-12 07:12:32 -0300

    :fire: chore: sql comand finished and atribuing into column of database

--- jdbc3(inserindo-dados)/src/application/Program.java
+++ jdbc3(inserindo-dados)/src/application/Program.java
@@ -1,37 +1,33 @@
 package application;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.text.SimpleDateFormat;
 
 import db.DB;
+
 
 public class Program {
 
 	public static void main(String[] args) {
-
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Connection conn = null;
-		Statement st = null;
-		ResultSet rs = null;
+		PreparedStatement st = null;
 		try {
 			conn = DB.getConnection();
-	
-			st = conn.createStatement();
 			
-			rs = st.executeQuery("select * from department");
+			st = conn.prepareStatement(
+				"INSERT INTO seller "
+					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES "
+					+ "(?, ?, ?, ?, ?)");
 			
-			while (rs.next()) {
-				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
-			}
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
-		finally {
-			DB.closeResultSet(rs);
-			DB.closeStatement(st);
-			DB.closeConnection();
+			st.setString(1, "Carl Purple");
+			st.setString(2, "carl@gmail.com");
+			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+			st.setDouble(4, 3000.0);
+			st.setInt(5, 4);
 		}
 		
 	}
