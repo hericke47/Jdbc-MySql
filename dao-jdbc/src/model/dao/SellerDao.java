142f70a58858c7989b0b40d758c96d18e180785a
Herick Exterkoetter <herickherick47@gmail.com>
2020-10-14 10:21:04 -0300

    :fire: chore: implementation of insert in Sellerdaojdbc

--- dao-jdbc/src/model/dao/SellerDao.java
+++ dao-jdbc/src/model/dao/SellerDao.java
@@ -7,10 +7,11 @@ import model.entities.Seller;
 
 public interface SellerDao {
 	
-	void insert(Seller obg);
+	void insert(Seller obj);
 	void update(Seller obj);
 	void deleteById(Integer id);
 	Seller findById(Integer id);
 	List<Seller> findAll();
 	List<Seller> findByDepartment(Department department);
+
 }
